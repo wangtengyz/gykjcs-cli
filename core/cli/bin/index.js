e46cf2387d381e188a3416ed150581411a1dd8fe
tengye <742132694@qq.com>
2022-05-14 01:12:09 +0800

    fix: 修改bug

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -4,7 +4,7 @@ const importLocal = require('import-local');
 // __dirname 表示当前文件所在的目录的绝对路径
 // __filename 表示当前文件的绝对路径
 if(importLocal(__filename)) {
-  rquire('npmlog').info('cli', '正在使用gykjcs-cli-dev本地版本')
+  require('npmlog').info('cli', '正在使用gykjcs-cli-dev本地版本')
 } else {
   require('../lib')(process.argv.slice(2))
 }
